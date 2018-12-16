94335c05cf89a948b10e5af713cac031f03dc759
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-12-16 08:53:27 +0800

    Add configuration for rspec in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,16 @@ module FlightBooker
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
