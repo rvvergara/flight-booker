1544d5ebbe364136552ef113f3342066d32dcc21
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-12-16 13:24:21 +0800

    Refactor flights_controller.rb

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,8 +3,12 @@ class FlightsController < ApplicationController
     @airports = Airport.list
     @dates = Flight.distinct_dates.ascending_dates.map {|flight| flight.date_formatted}
     
-      @origin = Airport.find_by(code:params[:origin]) if params[:origin]
-      @destination = Airport.find_by(code:params[:destination]) if params[:destination]
-      @flights = Flight.search_flights(@origin.id, @destination.id, params[:date]) if params[:date]
+    if params[:origin] && params[:destination] && params[:date]
+      @origin = Airport.find_by(code:params[:origin])
+      @destination = Airport.find_by(code:params[:destination])
+      @flights = Flight.search_flights(@origin.id, @destination.id, params[:date])
+    end
+    
    end
+
 end
