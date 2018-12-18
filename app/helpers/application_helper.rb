54e35ff33371b23a3d9a6403c0fb13e21b34998e
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-12-18 10:24:10 +0800

    Initial frontend work
    1. Add full_title method in application helper
    2. Include bootstrap container div in application.html.erb
    3. Create a header partial in layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  # Puts the page title in every page of the app
+  def full_title(page_title)
+    page_title.empty? ? "Flight Right" : "#{page_title} | Flight Right"
+  end
 end
