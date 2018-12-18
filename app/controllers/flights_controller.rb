5e68464ec2c8c4ad06da55f3e3b65f12ce23ed17
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-12-18 12:50:49 +0800

    Add bootstrap class on flights index
    1. Refactor flights_controller - create a flights helper method called
    search_on? to transfer logic of whether there is search result or not
    2. Divide home page and search results page by bootstrap cols

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,14 +2,16 @@ class FlightsController < ApplicationController
   def index
     @airports = Airport.list
     @dates = Flight.dates
-    
-    if params[:origin] && params[:destination] && params[:date] && params[:passenger_count]
+    @title = "Home"
+    if search_on?
       @origin = Airport.find_by(code:params[:origin])
       @selected_origin = @origin.code
       @destination = Airport.find_by(code:params[:destination])
       @selected_destination = @destination.code
       @flights = Flight.search_flights(@origin.id, @destination.id, params[:date])
       @passenger_count = params[:passenger_count]
+      @selected_date = params[:date]
+      @title = "Search Results"
     end
 
    end
--- app/helpers/flights_helper.rb
+++ app/helpers/flights_helper.rb
@@ -1,2 +1,5 @@
 module FlightsHelper
+  def search_on?
+    params[:origin] && params[:destination] && params[:date] && params[:passenger_count]
+  end
 end
