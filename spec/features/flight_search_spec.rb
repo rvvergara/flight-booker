527001f927dcb4116dbde9f3af26b67c57f41ca5
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-12-18 17:58:55 +0800

    Rename user_visits.. spec into flight_search_spec

--- spec/features/flight_search_spec.rb
+++ spec/features/flight_search_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature 'user visits the home page' do
+  
   before :each do
     visit root_path
   end
@@ -27,30 +28,25 @@ RSpec.feature 'user visits the home page' do
   end
 
   feature "user tries to search for flights" do
-    
-    scenario "user's choice of origin, destination and date corresponds to at least one existing flight" do
+    before :each do
       select("San Francisco International", from:"origin")
       select("New York, La Guardia Airport", from:"destination")
       select("1", from:"passenger_count")
       select("Sunday December-23-2018", from:"date")
       click_on("Search Flights")
+    end
     
+    scenario "user's choice of origin, destination and date corresponds to at least one existing flight" do   
       within "ul" do
         expect(page).to have_content("STOLA")
         expect(page).to have_content("STOLA2")
         expect(page).to have_selector(:link_or_button, "Select Flight")
       end
     end
+
     scenario "user clicks on a flight to book" do
-      select("San Francisco International", from:"origin")
-      select("New York, La Guardia Airport", from:"destination")
-      select("1", from:"passenger_count")
-      select("Sunday December-23-2018", from:"date")
-      click_on("Search Flights")
       first(:link_or_button, "Select Flight").click
-      
       expect(page).to have_content("Booking Form")
     end
   end
-
 end
